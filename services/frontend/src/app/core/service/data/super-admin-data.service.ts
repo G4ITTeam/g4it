21d9940f34bc80181faa4714cf9ba06ecea06549
TUSHAR <tushar.tushar2@soprasteria.com>
2025-04-03 12:06:18 +0530

    fix(1071): fix url

--- services/frontend/src/app/core/service/data/super-admin-data.service.ts
+++ services/frontend/src/app/core/service/data/super-admin-data.service.ts
@@ -18,6 +18,6 @@ export class SuperAdminDataService {
     constructor(private http: HttpClient) {}
 
     migrateDataToNewFormat(): Observable<void> {
-        return this.http.post<void>(`${endpoint}/evaluate-all-digital-services`, {});
+        return this.http.post<void>(`${endpoint}/evaluate-all-ds-data`, {});
     }
 }
