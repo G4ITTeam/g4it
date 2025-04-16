d51bf5c3f29855f66782a36ad9eb8dd7f2abae77
Vineeta Verma <vineeta.verma@soprasteria.com>
2025-04-16 13:59:29 +0530

    feat(1002): add a button to create its own personnal organization

--- services/backend/src/main/java/com/soprasteria/g4it/backend/apiuser/controller/UserRestController.java
+++ services/backend/src/main/java/com/soprasteria/g4it/backend/apiuser/controller/UserRestController.java
@@ -7,11 +7,16 @@
  */
 package com.soprasteria.g4it.backend.apiuser.controller;
 
+import com.soprasteria.g4it.backend.apiadministrator.business.AdministratorOrganizationService;
 import com.soprasteria.g4it.backend.apiuser.business.AuthService;
+import com.soprasteria.g4it.backend.apiuser.mapper.OrganizationRestMapper;
 import com.soprasteria.g4it.backend.apiuser.mapper.UserRestMapper;
 import com.soprasteria.g4it.backend.server.gen.api.UserApiDelegate;
+import com.soprasteria.g4it.backend.server.gen.api.dto.OrganizationRest;
+import com.soprasteria.g4it.backend.server.gen.api.dto.OrganizationUpsertRest;
 import com.soprasteria.g4it.backend.server.gen.api.dto.UserRest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -33,11 +38,23 @@ public class UserRestController implements UserApiDelegate {
     @Autowired
     private UserRestMapper userRestMapper;
 
+    @Autowired
+    AdministratorOrganizationService administratorOrganizationService;
+
+    @Autowired
+    private OrganizationRestMapper organizationRestMapper;
+
     /**
      * {@inheritDoc}
      */
     @Override
     public ResponseEntity<UserRest> getUser() {
         return ResponseEntity.ok(userRestMapper.toDto(authService.getUser()));
+    }
+
+    @Override
+    public ResponseEntity<OrganizationRest> createNewOrganization(OrganizationUpsertRest organizationUpsertRest) {
+        return new ResponseEntity<>(organizationRestMapper.toDto(administratorOrganizationService.createOrganization(organizationUpsertRest, authService.getAdminUser())),
+                HttpStatus.OK);
     }
 }
